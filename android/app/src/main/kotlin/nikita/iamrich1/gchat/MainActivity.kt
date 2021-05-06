9debd13383810bd6251a781c22c2ead691977dcf
Nikita Nand <nandmandal1998@gmail.com>
2021-05-06 16:46:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/nikita/iamrich1/gchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package nikita.iamrich1.gchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:gchat/welcome_screen.dart';
+import 'package:gchat/login_screen.dart';
+import 'package:gchat/registration_screen.dart';
+import 'package:gchat/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
